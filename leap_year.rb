7b727a4f0ef1319360d83ad7012c00e0a40917b2
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-10-14 21:46:18 +0000

    Deled instruction comments

--- leap_year.rb
+++ leap_year.rb
@@ -5,19 +5,6 @@ years = [
 ]
 year = years.sample
 
-=begin
-1. If the year is divisible by 4, go to step 2. Otherwise, go to step 5.
-2. If the year is divisible by 100, go to step 3. Otherwise, go to step 4.
-3. If the year is divisible by 400, go to step 4. Otherwise, go to step 5.
-4. The year is a leap year (it has 366 days).
-5. The year is not a leap year (it has 365 days).
-=end
-
-# Divisible by 4 + divisible by 100 + divisible by 400 --> leap year
-# Divisible by 4 + divisible by 100 + not divisible by 400 --> not leap year
-# Divisible by 4 + not divisible by 100 --> leap year
-# Not divisible by 4 --> Not leap year
-
 leap_year = "#{year} is a leap year!"
 not_leap_year = "#{year} is not a leap year."
 
