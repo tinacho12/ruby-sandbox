fb32cad8f7a106f6a736b6e65fed3a383bf6a2f4
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-10-14 20:59:25 +0000

    Deleted user input portion

--- twofer.rb
+++ twofer.rb
@@ -1,13 +1,9 @@
 name = ["raghu", "Bob", ""].sample
 
-# Ask user for name and save to a variable
-pp "What's your name?"
-user_name = gets
-
-# If no name given, print "One for you, one for me."
-if user_name == ""
+# If no name, print "One for you, one for me."
+if name == ""
   pp "One for you, one for me."
 else
   # If given name, capitalize it
-  puts "One for #{user_name.capitalize.chomp}, one for me.".chomp
+  puts "One for #{name.capitalize}, one for me."
 end
