4c107690c46862fb8a54a6606f1589f9c76ed9cb
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-10-15 17:05:08 +0000

    Swapped method to regex

--- secret_encoder.rb
+++ secret_encoder.rb
@@ -6,6 +6,8 @@ secret = [
 ].sample
 
 # write your program below
+
+=begin
 secret = secret.downcase
 secret = secret.gsub("a", "1")
 secret = secret.gsub("e", "2")
@@ -14,3 +16,6 @@ secret = secret.gsub("o", "4")
 secret = secret.gsub("u", "5")
 
 puts secret
+=end
+
+pp secret.gsub(/['1']/i, 'a').gsub(/['2']/i, 'e').gsub(/['3']/i, 'i').gsub(/['4']/i, 'o').gsub(/['5']/i, 'u')
